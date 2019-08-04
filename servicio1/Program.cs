67bed7e2908c28b0698aadaa985b9414b99a6f28
Luis Correa <alberto13711@gmail.com>
2019-08-04 14:16:22 -0500

    Cambio

---
+++ servicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace servicio1
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        public static void Main()
+        {
+#if (!DEBUG)
+
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+
+
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new Service1()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+
+        }
+    }
+}
---
+++ servicio1/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace servicio1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //AddingNewEventHandler timer1, AddressOf(HagoAlgo)
+            timer1.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //StreamWriter wt = new StreamWriter();
+            Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy")}");
+        }
+    }
+}
